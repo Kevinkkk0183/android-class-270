d3226ca5a0789b5c50e6cee41a8f8f032800f494
Kevinkkk0183 <hsuk1011@gmail.com>
2016-07-13 10:44:36 +0800

    add listview and spinner

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -4,32 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 創造物件
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
-
+    ListView listView;
+    Spinner spinner;
     String selectedTea = "Black Tea";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+         //讓程式取找到物件
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        listView = (ListView) findViewById(R.id.listView);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)
+                {
                     submit(v);
                     return true;
                 }
@@ -40,13 +49,35 @@ public class MainActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton =(RadioButton) group.findViewById(checkedId);
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
                 selectedTea = radioButton.getText().toString();
-                }
+            }
 
-            
         });
+        setUpListView();
+        setupSpinner();
     }
+
+
+
+
+    //定義物件
+
+    public void setupSpinner()
+    {
+        String[] data = getResources().getStringArray(R.array.storeInfos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,data); //this represent main activity
+        spinner.setAdapter(adapter);
+    }
+    
+
+    public void setUpListView()
+    {
+        String[] data = new String[]{"Black Tea", "Green Tea","1","2","3","4","5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,data); //this
+        listView.setAdapter(adapter);
+    }
+
 
     public void submit(View view) {
         String text = editText.getText().toString();
