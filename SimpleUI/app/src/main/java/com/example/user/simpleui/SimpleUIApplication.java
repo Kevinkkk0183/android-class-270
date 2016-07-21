4d1be9590254b9de9db9e2ae0ed2dd5f205d72f0
Kevinkkk0183 <hsuk1011@gmail.com>
2016-07-21 11:23:22 +0800

    add google map fragment

--- SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -16,9 +16,9 @@ public class SimpleUIApplication extends Application {
         ParseObject.registerSubclass(Order.class); //標明此物件是可以上傳的
         ParseObject.registerSubclass(Drink.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
-                        .applicationId("76ee57f8e5f8bd628cc9586e93d428d5")
-                        .server("http://parseserver-ps662-env.us-east-1.elasticbeanstalk.com/parse/")
-                        //.clientKey("FGYIC6Z7VTMLFMyWo1RUoMNNT7WUePa5ngxXCLSr")
+                        .applicationId("xDTUNne92ZNeldPcTrUPMT9RUowQwC7KxR7aLGx7")
+                        .server("https://parseapi.back4app.com/")
+                        .clientKey("FGYIC6Z7VTMLFMyWo1RUoMNNT7WUePa5ngxXCLSr")
                         .enableLocalDataStore()  //開啟local database的功能，沒有網路的情況下亦可儲存
                         .build()
 
